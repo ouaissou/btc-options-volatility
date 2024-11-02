58c0b2ac2aa542ef8ced9ac2d3c708f5cc89d09c
Wei Su <s_wei13@live.concordia.ca>
2024-11-02 17:50:46 -0400

    Initial project setup

---
+++ binance-options-volatility/src/main/java/com/wei/binance/binance_options_volatility/BinanceOptionsVolatilityApplication.java
@@ -0,0 +1,13 @@
+package com.wei.binance.binance_options_volatility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinanceOptionsVolatilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinanceOptionsVolatilityApplication.class, args);
+	}
+
+}
---
+++ binance-options-volatility/src/test/java/com/wei/binance/binance_options_volatility/BinanceOptionsVolatilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wei.binance.binance_options_volatility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinanceOptionsVolatilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
